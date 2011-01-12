1be6cc61f08a8f440b643ed60812dd2f35e7afcd
Loren Segal <lsegal@soen.ca>
2011-01-11 23:01:13 -0500

    Ensure dummy encoding is always returned

--- ext/backports/ruby/encoding.h
+++ ext/backports/ruby/encoding.h
@@ -31,13 +31,13 @@ int rb_char_to_option_kcode(int c, int *option, int *kcode) { return 0; }
 #define rb_enc_str_new(s,l,e) rb_str_new(s,l)
 #define rb_enc_reg_new(s,l,e,i) rb_reg_new(s,l,i)
 #define rb_external_str_new_with_enc(s,l,e) rb_str_new(s,l)
-#define rb_enc_from_index(idx) 0
-#define rb_enc_find(name) 0
+#define rb_enc_from_index(idx) __default_encoding
+#define rb_enc_find(name) __default_encoding
 #define rb_enc_name(enc) (enc)->name
 #define rb_enc_mbminlen(enc) (enc)->min_enc_len
 #define rb_enc_mbmaxlen(enc) (enc)->max_enc_len
-#define rb_enc_mbclen(p,e,enc) (e-p)
-#define rb_enc_fast_mbclen(p,e,enc) (e-p)
+#define rb_enc_mbclen(p,e,enc) 1
+#define rb_enc_fast_mbclen(p,e,enc) 1
 #define rb_enc_isascii(c,enc) isascii(c)
 #define rb_enc_isalpha(c,enc) isalpha(c)
 #define rb_enc_islower(c,enc) islower(c)
@@ -49,7 +49,7 @@ int rb_char_to_option_kcode(int c, int *option, int *kcode) { return 0; }
 #define rb_enc_isdigit(c,enc) isdigit(c)
 #define rb_enc_toupper(c,e) toupper(c)
 #define rb_enc_tolower(c,e) tolower(c)
-#define rb_enc_str_asciicompat_p(str) rb_enc_asciicompat(rb_enc_get(str))
+#define rb_enc_str_asciicompat_p(str) 1
 #define rb_enc_from_encoding(e) Qnil
 #define rb_enc_precise_mbclen(s,l,e) 1
 #define rb_enc_unicode_p(e) 0
